ffc93742db63b61cc1a40b3a60c1c942d5b42a3d
dlithepurushottam <dlithepurushottam@gmail.com>
2025-05-22 12:08:27 +0530

    updated with fetch user by ID route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,6 +21,20 @@ class UserController{
         }
     }
 
+
+    async getUserById(req,res){
+        try{
+           const user = await userService.getUserById(userId);
+           if(!user)
+              return res.status(404).json({error: "User not found"});
+            res.json(user);  
+        }catch(error){
+           res.status(500).json({error:error.message}); 
+        }
+    }
+
+
+
 }
     
   
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,4 +7,6 @@ router.post("/", userController.createUser);
 
 router.get("/",userController.getAllUsers);
 
+router.get("/:id",userController.getUserById);
+
 module.exports=router;
